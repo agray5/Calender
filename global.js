365da5950fa0de40b2d6e2dda8ab3cf896ff9cbf
Unknown <grayaprild@gmail.com>
2017-08-12 21:03:43 -0500

    Moved around code into diffrent files. Changed how menu is cleared. Menu close button is fixed. Saving an event is fixed.

---
+++ global.js
@@ -0,0 +1,20 @@
+// selected date
+let selectedDate = new Date();
+let selectedDateTD = "none";
+// current date
+let currentDate = new Date();
+
+// stored events
+let events = [];
+
+//events
+let eventCnt = 0;
+
+//selected event
+let selectedEvent = "none";
+
+//stored stickers
+let stickers = [];
+
+//current frame
+let frame = {};
